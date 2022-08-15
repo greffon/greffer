45a2cfbe7f1824b5297571a84793a52dfe8a1cdc
Omar Sy <syomar.pro@gmail.com>
2022-08-16 01:06:40 +0200

    feat: not crash when we have a monitoring error

--- apps/utils/greffon/monitoring.py
+++ apps/utils/greffon/monitoring.py
@@ -6,14 +6,19 @@ import logging
 from django.conf import settings
 logger = logging.getLogger(settings.LOGGER_NAME)
 
+
 def monitor_status(delay=5):
     greffon_dir = os.getenv('GREFFON_PATH')
     prev_status = {}
-    while True:
-        logger.info("monitoring begin")
-        for greffon_id in os.listdir(greffon_dir):
-            status = compose.get_status(greffon_id)['status']
-            if prev_status.get(greffon_id) != status:
-                base_server.change_status(greffon_id, status)
-            prev_status[greffon_id] = status
-        time.sleep(delay)
+    try:
+        while True:
+            logger.info("monitoring begin")
+            for greffon_id in os.listdir(greffon_dir):
+                status = compose.get_status(greffon_id)['status']
+                if prev_status.get(greffon_id) != status:
+                    base_server.change_status(greffon_id, status)
+                prev_status[greffon_id] = status
+            time.sleep(delay)
+    except Exception as e:
+        logger.error(e)
+        time.sleep(delay*2)
